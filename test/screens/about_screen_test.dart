ba65de509bef2a10eff118ec8e476ee4d2917e92
Marco Pinto <marco.alp120@gmail.com>
2021-07-15 14:33:03 +0100

    Fixed indentation in about screen test
    - Removed part of test name and ran flutter format;

--- test/screens/about_screen_test.dart
+++ test/screens/about_screen_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:wingscore/screens/about_screen.dart';
 
 void main() {
-  testWidgets('[screens] about screen initial state is correct', (tester) async {
+  testWidgets('[screens] about screen initial state', (tester) async {
     await tester.pumpWidget(MaterialApp(home: AboutScreen()));
 
     expect(find.text('About'), findsOneWidget);
